38a3800aabf3fafb364b71628c47b7a611e26227
Marten Sievers <ms@tjek.com>
2022-07-03 13:44:31 +0200

    chore: update to the latest version of external dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@
 */
 
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const env = require('dotenv').config({path: 'variables.env'});
+const nodeEnv = process.env.NODE_ENV || env.parsed.NODE_ENV;
+
 /*
   webpack sees every file as a module.
   How to handle those files is up to loaders.
@@ -15,10 +16,13 @@ const autoprefixer = require('autoprefixer');
 // This is our JavaScript rule that specifies what to do with .js files
 const javascript = {
   test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
-  use: [{
+  exclude: /(node_moduels)/,
+  use: {
     loader: 'babel-loader',
-    options: { presets: ['env'] } // this is one way of passing options
-  }],
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  },
 };
 
 /*
@@ -28,7 +32,9 @@ const javascript = {
 const postcss = {
   loader: 'postcss-loader',
   options: {
-    plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
+    postcssOptions: {
+      plugins: { autoprefixer: true}
+    }
   }
 };
 
@@ -38,22 +44,35 @@ const styles = {
   // here we pass the options as query params b/c it's short.
   // remember above we used an object for each loader instead of just a string?
   // We don't just pass an array of loaders, we run them through the extract plugin so they can be outputted to their own .css file
-  use: ExtractTextPlugin.extract(['css-loader?sourceMap', postcss, 'sass-loader?sourceMap'])
+  use: [
+    MiniCssExtractPlugin.loader,
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true
+      }
+    },
+    {
+      loader: 'resolve-url-loader',
+    },
+      postcss,
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true
+      }
+    }
+  ]
 };
-
-// We can also use plugins - this one will compress the crap out of our JS
-const uglify = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
-  compress: { warnings: false }
-});
 
 // OK - now it's time to put it all together
 const config = {
+  mode: nodeEnv,
+  devtool: 'source-map',
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
     App: './public/javascripts/delicious-app.js'
   },
-  // we're using sourcemaps and here is where we specify which kind of sourcemap to use
-  devtool: 'source-map',
   // Once things are done, we kick it out to a file.
   output: {
     // path is a built in node module
@@ -72,10 +91,8 @@ const config = {
   // plugins: [uglify]
   plugins: [
     // here is where we tell it to output our css to a separate file
-    new ExtractTextPlugin('style.css'),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
   ]
 };
-// webpack is cranky about some packages using a soon to be deprecated API. shhhhhhh
-process.noDeprecation = true;
 
 module.exports = config;
